937dbc11189872819ee2d100fbf2d5407938d7a7
Andrew Ottaviano <andrew_o1995@live.com>
2023-05-02 23:28:20 -0500

    Trying Vercel

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -30,7 +30,7 @@ export default function App() {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
           rel="stylesheet"
-        ></link>
+        ></link> */}
       </head>
       <body style={{ margin: 0 }}>
         <Outlet />

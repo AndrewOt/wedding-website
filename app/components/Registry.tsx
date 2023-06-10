a1363abfa501d762f88df903d441314db6889eab
Andrew Ottaviano <andrew_o1995@live.com>
2023-06-10 16:23:42 -0500

    Added registry links and logos

--- app/components/Registry.tsx
+++ app/components/Registry.tsx
@@ -2,7 +2,31 @@ export const Registry = () => {
   return (
     <section className="section registry">
       <h1>Gift Registry</h1>
-      <h3>Coming soon!</h3>
+      <div style={{ display: "flex", gap: "1rem", justifyContent: "center" }}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.amazon.com/wedding/andrew-ottaviano-lydia-helt--august-2023/registry/2G11L74SGRL8N"
+        >
+          <img
+            alt="Amazon registry"
+            style={{ width: "5rem" }}
+            src="https://i1.wp.com/www.santacruztechbeat.com/wp-content/uploads/2015/04/Amazon_logo-8.png"
+          />
+        </a>
+
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.target.com/gift-registry/gift/andrewandlydia"
+        >
+          <img
+            alt="Amazon registry"
+            style={{ width: "5rem" }}
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogodownload.org%2Fwp-content%2Fuploads%2F2020%2F11%2Ftarget-logo-2.png&f=1&nofb=1&ipt=e7a0a1ac64f414c51b72dd0c35315657b1468c3d3aa7de9a12f3cae48acea590&ipo=images"
+          />
+        </a>
+      </div>
     </section>
   );
 };

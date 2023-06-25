8a696cff6d9bdb28e699da9e0627a8a1894abe2e
Andrew Ottaviano <andrew_o1995@live.com>
2023-06-25 18:40:20 -0500

    updated engagement photos

--- app/components/Photos.tsx
+++ app/components/Photos.tsx
@@ -1,15 +1,61 @@
 import AliceCarousel from "react-alice-carousel";
-import 'react-alice-carousel/lib/alice-carousel.css'
+import "react-alice-carousel/lib/alice-carousel.css";
 
-const handleDragStart = (e: React.DragEvent<HTMLImageElement>) => e.preventDefault();
+const handleDragStart = (e: React.DragEvent<HTMLImageElement>) =>
+  e.preventDefault();
 
 const items = [
-  <img src="engagement1.jpg" className="engagement-picture" onDragStart={handleDragStart} role="presentation" />,
-  <img src="engagement2.jpg" className="engagement-picture" onDragStart={handleDragStart} role="presentation" />,
-  <img src="engagement3.jpg" className="engagement-picture" onDragStart={handleDragStart} role="presentation" />,
+  <img
+    key="engagement1.jpg"
+    src="engagement1.jpg"
+    alt="Andrew proposes to Lydia!"
+    className="engagement-picture"
+    onDragStart={handleDragStart}
+    role="presentation"
+  />,
+  <img
+    src="engagement_shoot1.jpg"
+    key="engagement_shoot1.jpg"
+    alt="Andrew and Lydia"
+    className="engagement-picture"
+    onDragStart={handleDragStart}
+    role="presentation"
+  />,
+  <img
+    src="engagement2.jpg"
+    key="engagement2.jpg"
+    alt="Andrew and Lydia"
+    className="engagement-picture"
+    onDragStart={handleDragStart}
+    role="presentation"
+  />,
+  <img
+    src="engagement_shoot2.jpg"
+    key="engagement_shoot2.jpg"
+    alt="Andrew and Lydia"
+    className="engagement-picture"
+    onDragStart={handleDragStart}
+    role="presentation"
+  />,
+  <img
+    src="engagement4.jpg"
+    key="engagement4.jpg"
+    alt="Andrew and Lydia"
+    className="engagement-picture"
+    onDragStart={handleDragStart}
+    role="presentation"
+  />,
+  <img
+    key="engagement_shoot3.jpg"
+    src="engagement_shoot3.jpg"
+    alt="Andrew and Lydia"
+    className="engagement-picture"
+    onDragStart={handleDragStart}
+    role="presentation"
+  />,
 ];
 
-const responsive = { 0: { items: 1 }};
+const responsive = { 0: { items: 1 } };
 
 export const Photos = () => (
   <div>
